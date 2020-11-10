b92cc1b1cc203823e0ffdc3898377db7b2551b48
Manuel Armas <manu12694@gmail.com>
2020-11-09 21:08:45 -0400

    agregado el ejercicio de canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,7 +1,19 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
+for (l = 0; l < lineas; l++) {
 
-dibujarLinea("pink", 10, 300, 220, 10);
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("linea " + l);
+
+}
+dibujarLinea(colorcito, 1, 1, 1, 300);
+dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea(color, xincial, yinicial, xfinal, yfinal) {
     //arrancamos a dibujar un camino
