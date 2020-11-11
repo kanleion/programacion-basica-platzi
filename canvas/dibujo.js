d8bf5785155cbfae9e591f04e3c7989d3f3a6ae2
Manuel Armas <manu12694@gmail.com>
2020-11-10 21:48:47 -0400

    agregamos ya la opción de que el usuario puede colocar su propia cantidad de líneas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,19 +1,13 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+//aquí estamos referenciando una función, que la va aejecutar cuando se ejecute
+boton.addEventListener("click", dibujoPorClic);
+
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "#FAA";
-for (l = 0; l < lineas; l++) {
+var ancho = d.width;
 
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-    console.log("linea " + l);
-
-}
-dibujarLinea(colorcito, 1, 1, 1, 300);
-dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea(color, xincial, yinicial, xfinal, yfinal) {
     //arrancamos a dibujar un camino
@@ -23,5 +17,25 @@ function dibujarLinea(color, xincial, yinicial, xfinal, yfinal) {
     lienzo.lineTo(xfinal, yfinal); //Teminamos de dibujar en el punto 200,200
     lienzo.stroke(); //cerramos la linea
     lienzo.closePath(); //dejamos de dibujar
+
+}
+
+function dibujoPorClic() {
+
+    var lineas = parseInt(texto.value);
+    var l = 0;
+    var yi, xf;
+    var colorcito = "#FAA";
+    var espacio = ancho / lineas;
+    for (l = 0; l < lineas; l++) {
+
+        yi = espacio * l;
+        xf = espacio * (l + 1);
+        dibujarLinea(colorcito, 0, yi, xf, 300);
+        console.log("linea " + l);
+
+    }
+    dibujarLinea(colorcito, 1, 1, 1, 300);
+    dibujarLinea(colorcito, 1, 299, 299, 299);
 
 }
