744df244df36902b59bd2c5541a753d22e854793
Manuel Armas <manu12694@gmail.com>
2020-11-14 11:59:57 -0400

    listo el reto de  dibujar varias cantidades de cerdos y pollos

--- villa-platzi/platzi.js
+++ villa-platzi/platzi.js
@@ -12,6 +12,16 @@ var fondo = {
     cargaOk: false
 };
 
+var cerdo = {
+    url: "cerdo.webp",
+    cargaOk: false
+};
+
+var pollo = {
+    url: "pollo.webp",
+    cargaOk: false
+};
+
 var cantidad = aleatorio(5, 25);
 
 //crear objeto, cuando ustedes tienen la definición de un objeto, se les llaman clases y 
@@ -23,6 +33,15 @@ fondo.imagen.addEventListener("load", cargarFondo);
 vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
 /*
 var cerdo = new Image();
 cerdo.src = "cerdo.webp";
@@ -44,16 +63,24 @@ function cargarVacas() {
     dibujar();
 }
 
-function dibujar() {
+function cargarCerdos() {
+    cerdo.cargaOk = true;
+    dibujar();
+}
 
+function cargarPollos() {
+    pollo.cargaOk = true;
+    dibujar();
+}
+
+function dibujar() {
+    //dibujo del fondo
     if (fondo.cargaOk) {
         papel.drawImage(fondo.imagen, 0, 0);
     }
-
+    //dibujo de las vacas
     if (vaca.cargaOk) {
-
-
-        console.log(cantidad);
+        console.log("El número de vacas es: " + cantidad);
         for (var v = 0; v < cantidad; v++) {
             var x = aleatorio(0, 7);
             var y = aleatorio(0, 7);
@@ -63,7 +90,32 @@ function dibujar() {
 
         }
     }
+    //dibujo de los cerdos
+    if (cerdo.cargaOk) {
+        var cantidadCerdo = aleatorio(2, 9);
+        console.log("El número de cerdos es: " + cantidadCerdo);
+        for (var v = 0; v < cantidadCerdo; v++) {
+            var x = aleatorio(0, 7);
+            var y = aleatorio(0, 7);
+            var x = x * 60;
+            var y = y * 60;
+            papel.drawImage(cerdo.imagen, x, y);
 
+        }
+    }
+    //dibujo de los pollos
+    if (pollo.cargaOk) {
+        var cantidadPollo = aleatorio(1, 5);
+        console.log("El número de pollos es: " + cantidadPollo);
+        for (var v = 0; v < cantidadPollo; v++) {
+            var x = aleatorio(0, 7);
+            var y = aleatorio(0, 7);
+            var x = x * 60;
+            var y = y * 60;
+            papel.drawImage(pollo.imagen, x, y);
+
+        }
+    }
 }
 /*
 function dibujarVacas() {
